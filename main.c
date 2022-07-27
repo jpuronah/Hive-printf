ada17c1c8bf952e70e48d1f5663580e8693016de
jpuronah <jpuronah@student.hive.fi>
2022-07-27 15:00:17 +0300

    Repo cleaned, check notes.txt

--- main.c
+++ main.c
@@ -3,43 +3,14 @@
 
 int	main(int ac, char **av)
 {
-	char		*str;
-	int			integer;
-	double		dubbel;
-	int			return_value;
-	char		param;
-	t_printf	*flags;
-	int			ret;
-
-	ret = 0;
-	integer = 0;
-	dubbel = 1230.000865;
-	integer = 5512;
-	str = "sana";
-	char	*strnul = NULL;
-	char	*strnul2 = 0;
-	char	*strnul3 = "";
-	char	*string = "string";
-	long long longi = 321321321321213321;
-
 	printf("own printf:\n");
-	//ret = ft_printf("%Lf", 1.42l);
 	ret = ft_printf("%f", -1.234);
 	printf("|\nret: %d\n", ret);
 	printf("\n**********************\n");
-	/*ft_printf("%c", 'a');
-	printf("|\n**********************\n");
-	ft_printf("%%");
-	printf("|\n**********************\n");*/
 
 	printf("\nreal printf:\n");
-	//ret = printf("%Lf", 1.42l);
 	ret = printf("%f", -1.234);
 	printf("|\nret: %d\n", ret);
 	printf("\n**********************\n");
-	/*return_value = ft_printf("%p, %%, %s, %d, %c, %i, %.1f", str, (const char *restrict)str, integer, str[2], integer / 2, dubbel);
-	printf("\noma ret: %d\n", return_value);
-	return_value = printf("%p, %%, %s, %d, %c, %i, %.1f", str, (const char *restrict)str, integer, str[2], integer / 2, dubbel);
-	printf("\noikee ret: %d\n", return_value);*/
 	return (0);
 }
